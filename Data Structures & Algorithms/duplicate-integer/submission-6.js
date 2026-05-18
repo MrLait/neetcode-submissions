c34e278d9a1ed9de3f76cf560aa713b9e25a1a4b
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-18 20:55:00 +0300

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const set = new Set();
+        const n = nums.length;
+
+        for(let i = 0; i < n; i++)
+        {
+            if(set.has(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
