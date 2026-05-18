a2d92f0e0c9f6a58f6dbcc6926127cf99ea3b48c
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-18 21:41:46 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+            return false;
+
+        var map = new Dictionary<char, int>();
+
+        for (var i = 0; i < s.Length; i++) {
+            map.TryGetValue(s[i], out int countS);
+            map[s[i]] = countS + 1;
+
+            map.TryGetValue(t[i], out  countS);
+            map[t[i]] = countS - 1;
+        }
+
+        return map.Values.All(v => v == 0);
+    }
+}
