d705d8f255e19072d76bb33c731763dd79f22f1e
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-18 22:07:04 +0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+            return false;
+
+        var arr = new int[26];
+
+        for (var i = 0; i < s.Length; i++) {
+            arr[s[i] - 'a'] --;
+            arr[t[i] - 'a'] ++;
+        }
+
+        return arr.All(v => v == 0);
+    }
+}
