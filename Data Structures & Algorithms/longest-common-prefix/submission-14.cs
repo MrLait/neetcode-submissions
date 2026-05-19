ba8af12a8419ed132ac2b9f563ac9b34a6631608
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-19 16:29:37 +0300

    Add: longest-common-prefix - submission-14

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-14.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+            return string.Empty;
+
+        int minLen = strs[0].Length;
+
+        for(int k = 1; k < strs.Length; k++)
+            minLen = Math.Min(minLen, strs[k].Length);
+
+        if(minLen == 0) return string.Empty;
+        
+        var prefix = new StringBuilder();
+
+        for(int j = 0; j < minLen; j++)
+        {
+            var cur  = strs[0][j];
+            
+            for(var i = 1; i < strs.Length; i++)
+            {
+                if (strs[i][j] != cur)
+                    return prefix.ToString();
+            }
+            
+            prefix.Append(cur);
+        }
+
+        return prefix.ToString();
+    }
+}
