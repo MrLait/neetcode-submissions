b85b4afe35cb170e2fb67c805db01424b91d71aa
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-19 19:07:45 +0300

    Add: longest-common-prefix - submission-18

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-18.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let prefix = strs[0];
+
+        for (let i = 1; i < strs.length; i++) {
+            let j = 0;
+            for (; j < Math.min(prefix.length, strs[i].length); j++) {
+                if (prefix[j] != strs[i][j]) {
+                    break;
+                }
+            }
+
+            prefix = prefix.substring(0, j);
+
+            if (prefix === "") break;
+        }
+
+        return prefix;
+    }
+}
