86e97c8d3613acd2803c55580b9075ef2824ea46
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-20 16:25:26 +0300

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        var k = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(val != nums[i])
+            {
+                nums[k] = nums[i];
+                k++;
+
+            }
+        }    
+            
+        return k;
+    }
+}
