88065912521bcbee47f16268cecbfbd1e87ca626
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-20 17:11:38 +0300

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        nums.sort();
+
+        return nums[Math.floor(nums.length / 2)];
+    }
+}
