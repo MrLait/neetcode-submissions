669b32499cc25e03adfec4d97e39f7ab2dcd56b9
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-22 10:06:02 +0300

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums)
+    {
+        if(nums.Length == 0) return 0;
+        if(nums.Length == 1) return 1;
+
+        var pq = new PriorityQueue<int,int>();
+        int maxSecuenceLen = 1, secuenceLen = 1;
+
+        foreach(var num in nums)
+            pq.Enqueue(num,num);
+        
+        int prev = pq.Dequeue(), cur = 0;
+        for(var i = 1; i < nums.Length; i++)
+        {
+            cur = pq.Dequeue();
+            if(prev == cur) continue;
+
+            if(prev + 1 == cur)
+            {
+                secuenceLen++;
+            }
+            else if(prev + 1 != cur)
+            {
+                secuenceLen = 1;
+            }
+            
+            maxSecuenceLen = Math.Max(maxSecuenceLen, secuenceLen);
+            prev = cur;
+        }
+        
+        return maxSecuenceLen;
+    }
+}
