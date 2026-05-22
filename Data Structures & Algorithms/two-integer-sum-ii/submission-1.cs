32673e55a5c1fa262162608351978ef64808d178
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-22 15:34:53 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var map = new Dictionary<int, int>();
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            var needed = target - numbers[i];  
+            if(map.ContainsKey(needed))
+            {
+                if(map[needed] < i && needed != numbers[i])
+                {
+                    return [map[needed] + 1, i + 1];
+                }
+            }
+            map[numbers[i]] = i;
+        }    
+        return [];
+    }
+}
