7bac3c074c814f8c9b88bbc70dcb33cf8221b93d
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-22 15:12:03 +0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        var j = s.Length - 1;
+        var i = 0;
+
+        while(i < j)
+        {    
+            while (!char.IsLetterOrDigit(s[i]) && i < j)
+                i++;
+            
+            while (!char.IsLetter(s[j]) && j > i) 
+                j--;
+
+            if (char.ToLower(s[i]) != char.ToLower(s[j]))
+                return false;
+            
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
