1eab952a8a4c38066a56e7cc48b58a4646c9bee3
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-23 20:05:37 +0300

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Search(int[] nums, int target)
+    {
+        int result = 0, l = 0, r = nums.Length - 1, mid = 0;
+
+        while(l <= r)
+        {
+            mid = (l + r) / 2;
+
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
