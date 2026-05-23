a09851518d8a9a583f4f907941b0c12c341877e7
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-23 16:28:20 +0300

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach(var c in s)
+        {
+
+            if(c == '(' || c ==  '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                var isEmpty = stack.TryPop(out char curr);
+                
+                if(!isEmpty) return false;
+
+                if(curr == '[' && c != ']')
+                    return false;
+                else if(curr == '{' && c != '}')
+                    return false;
+                else if(curr == '(' && c != ')')
+                    return false;
+            }
+        }
+
+        return stack.Count == 0 ? true : false; 
+    }
+}
