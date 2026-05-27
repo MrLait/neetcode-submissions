d8a98c509eeb026315aa6d4ae3336b5e433afcb9
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-27 19:40:15 +0300

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxProfit(int[] prices)
+    {
+        var result = 0;
+
+        for(int i = 1; i < prices.Length; i++)
+        {
+            if(prices[i] > prices[i - 1])
+                result += prices[i] - prices[i - 1];
+        }
+
+        return result;
+    }
+}
