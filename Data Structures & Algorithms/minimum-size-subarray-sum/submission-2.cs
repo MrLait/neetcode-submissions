2ca564238153b392a2cb020eeb05bfc441434fac
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2026-05-30 13:11:53 +0300

    Add: minimum-size-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-2.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        int slow = 0, prefixSum = 0;
+        var minSubArrLen = int.MaxValue;
+
+        for(var fast = 0; fast < nums.Length; fast++)
+        {
+            prefixSum += nums[fast];
+
+            while(prefixSum >= target && slow <= fast)
+            {
+                minSubArrLen = Math.Min(minSubArrLen, fast - slow);
+                prefixSum -= nums[slow++];
+            }
+        }
+
+        return minSubArrLen == int.MaxValue ? 0 : minSubArrLen + 1;
+    }
+}
